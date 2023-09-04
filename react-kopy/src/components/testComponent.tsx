542b9fff4c37773b8f9fe508db627797a46de9d2
darkchinax <darkchinax@gmail.com>
2023-09-04 21:09:33 +0300

    returned to stateless

--- react-kopy/src/components/testComponent.tsx
+++ react-kopy/src/components/testComponent.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
 import UserComponent from "./UserComponent";
 
 interface User {
@@ -7,36 +7,55 @@ interface User {
   email: string;
 }
 
-class Test extends Component {
-  constructor(props: any) {
-    super(props);
-    this.state = { users: [] };
-  }
+const Test = () => {
+  const [users, setUsers] = useState([]);
 
-  componentDidMount(): void {
+  useEffect(() => {
     fetch("http://127.0.0.1:3000/api/user")
       .then((res) => res.json())
       .then((data) => {
-        this.setState({ users: data.users });
+        setUsers(data.users);
       });
-  }
+  }, []);
 
-  render() {
-    const userComponentProps = {
-      name: "this.state.users[0].name",
-    };
-    console.log(this.state.users)
-    return (
-      <div>
-        <div>
-          <UserComponent {...userComponentProps} />
-          {/* {users.map((user) => {
-            <UserComponent  />;
-          })} */}
-        </div>
-      </div>
-    );
-  }
-}
+  const userComponentProps = {
+    name: "this.state.users[0].name",
+  };
+  return <UserComponent {...userComponentProps} />;
+};
 
 export default Test;
+
+// class Test extends Component {
+//   constructor(props: any) {
+//     super(props);
+//     this.state = { users: [] };
+//   }
+
+//   componentDidMount(): void {
+//     fetch("http://127.0.0.1:3000/api/user")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         this.setState({ users: data.users });
+//       });
+//   }
+
+//   render() {
+//     const userComponentProps = {
+//       name: "this.state.users[0].name",
+//     };
+//     console.log(this.state.users);
+//     return (
+//       <div>
+//         <div>
+//           <UserComponent {...userComponentProps} />
+//           {/* {users.map((user) => {
+//             <UserComponent  />;
+//           })} */}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Test;
